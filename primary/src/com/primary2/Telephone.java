2e94fd6c92e1826f8a721d11798ac1e9f40683ec
Toyz Stark <>
2019-04-09 22:31:50 +0800

    no message

---
+++ primary/src/com/primary2/Telephone.java
@@ -0,0 +1,13 @@
+package com.primary2;
+/*
+ * 用户介绍类的属性和方法定义过程
+ * 1.类是对象的类型；2.类的组成分为 属性和方法
+ * 2.定义类的步骤：1.定义类名（public class 类名）;2.编写类的属性（属性的类型 属性1）;3.编写类的方法（方法1）
+ */
+public class Telephone {
+	float screen;
+	
+	public static void main(String[] args) {
+
+	}
+}
